d544268ca01e95cc09b2b2576e0d55e4eba3b233
vergil <251627752@qq.com>
2020-07-19 11:01:20 +0800

    27. 移除元素

---
+++ RemoveElement.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+27. 移除元素
+给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+*/
+func main() {
+	var nums = []int{0, 1, 2, 2, 3, 0, 4, 2}
+	var val = 2
+	fmt.Println(Solution1(nums, val))
+}
+
+func Solution1(nums []int, val int) int {
+	var l = 0
+	for i := range nums {
+		if nums[i] != val {
+			nums[l] = nums[i]
+			l++
+		}
+	}
+	return l
+}
